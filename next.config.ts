5603640a9f28d72b9255d6c35a5592cf161da80a
sheona001 <sirexcc001@gmail.com>
2025-03-26 05:33:11 +0600

    fix commit

--- next.config.ts
+++ next.config.ts
@@ -3,5 +3,16 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 };
-
+module.exports = {
+  output: 'standalone', // For Docker optimization
+  experimental: {
+    serverComponentsExternalPackages: ['@turf/turf', 'leaflet'],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    domains: ['open-meteo.com'], // Add any image domains you use
+  }
+};
 export default nextConfig;
